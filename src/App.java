8f6063b9098e2ba68b5f2e60303bba49232aeee7
unknown <mayancelaevelyn8@.com>
2026-04-14 19:37:19 -0500

    UPD: App file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int[] array = new int[] {1,2,3,4,5};
+        System.out.println(array[0]);
+        System.out.println(array);
+        //for normal
+        for(int i=0; i<array.length; i++){
+            System.out.println(array[i]);
+        }
+        //for each
+        for(int i : array){
+            System.out.println(i);
+        }
     }
 }
